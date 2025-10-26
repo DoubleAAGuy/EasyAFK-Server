363859afd153cacbcf8d9be2b3e7aca6212607ab
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2025-10-26 19:51:28 -0400

    Refactor install.sh by removing unnecessary commands
    Removed git clone command and cleaned up script.

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-rm -rf *
-git clone https://github.com/DoubleAAGuy/EasyAFK-Server.git .
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.8/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
