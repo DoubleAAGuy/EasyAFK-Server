5355d3cb03b040e1fa34a844d9e5fc101256d6a0
DoubleAAGuy <142100304+DoubleAAGuy@users.noreply.github.com>
2025-10-26 21:16:22 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.8/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 22.21.0
-nvm use 22.21.0
-npm install mineflayer
